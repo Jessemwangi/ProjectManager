da0b3cce9d8e7c86bb6ca9a7e88db6c8504ce78f
jessemwangi <jessejzee@gmail.com>
2023-01-13 00:41:04 +0200

    todo done

--- src/Component/TodoList.tsx
+++ src/Component/TodoList.tsx
@@ -10,14 +10,53 @@ interface props{
 const TodoList:React.FC<props> = ({todoList,setTodos}) => {
     console.log(todoList)
     return (
-        <div className='todos'>
-                 {todoList.map(todo_ =>
+        <div className="container">
+            <div className="todos_active todos_block">
+                <span className="todos_headings">
+                    Todo Backlog
+                </span>
+                             {todoList.map(todo_ =>
             <SingleTodo todo={todo_} 
             key={todo_.id} todos={todoList}
             setTodos={setTodos} />
 
       )}
+            </div>
+
+            <div className="todos_ongoing todos_block">
+            <span className="todos_headings">
+                    Active Task
+                </span>
+                             {todoList.map(todo_ =>
+            <SingleTodo todo={todo_} 
+            key={todo_.id} todos={todoList}
+            setTodos={setTodos} />
+
+      )}
+            </div>
+
+            <div className="todos_complete todos_block">
+            <span className="todos_headings">
+                    Completed Task
+                </span>
+                             {todoList.map(todo_ =>
+            <SingleTodo todo={todo_} 
+            key={todo_.id} todos={todoList}
+            setTodos={setTodos} />
+
+      )}
+            </div>
         </div>
+    //     <div className='todos'>
+
+            
+    //              {todoList.map(todo_ =>
+    //         <SingleTodo todo={todo_} 
+    //         key={todo_.id} todos={todoList}
+    //         setTodos={setTodos} />
+
+    //   )}
+    //     </div>
     );
 };
 
